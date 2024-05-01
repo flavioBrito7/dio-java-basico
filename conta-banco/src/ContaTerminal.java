7cdc6cec40d60c3bdc2dd06e09b281175e21e686
flavioBrito <flavioandersondebrito@gmail.com>
2024-05-01 10:37:01 -0300

    alterações finais nas linhas de impressão

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
+        //Instanciando o objeto "Scanner" e criando atributos da classe "ContaTerminal".
         Scanner in = new Scanner(System.in, "latin1");
         int numero;
         String agencia;
         String nomeCliente;
-        double saldo;
+        double saldo = 237.48;
 
-        System.out.print("Por favor, digite o nome do cliente: ");
+        //Recebendo, via terminal, o dados da conta...
+        System.out.print("\n------------------------------------------------------\nPor favor, digite o nome do cliente: ");
         nomeCliente = in.nextLine();
-
         System.out.print("Por favor, digite o número da agência: ");
         agencia = in.next();
-
         System.out.print("Por favor, digite o número da conta: ");
         numero = Integer.parseInt(in.next());
 
-        System.out.print("Por favor, digite o número da agência: ");
+        //Encerrando a atividade da instância do objeto Scanner
+        in.close();
 
-
-        System.out.println("Olá" + nomeCliente + ", onrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque.");
+        System.out.println("------------------------------------------------------\nOlá, " + nomeCliente + "!\nObrigado por criar uma conta em nosso banco!\nA sua agência é " + agencia + " e a sua conta " + numero + ".\nSeu saldo de R$" + saldo + " já está disponível para saque.\n------------------------------------------------------");
         
     }
 }
