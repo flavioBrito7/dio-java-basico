b5be9a1c02490a55aac8bc723b54398003ea87d2
flavioBrito <flavioandersondebrito@gmail.com>
2024-05-19 12:05:48 -0300

    final

--- deafio-dio-poo/src/Main.java
+++ deafio-dio-poo/src/Main.java
@@ -1,7 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.dio.desafio.dominio.*;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        }
+            Curso curso1 = new Curso();
+            curso1.setTitulo("curso java");
+            curso1.setDescricao("descrição curso java");
+            curso1.setCargaHoraria(8);
+
+            Curso curso2 = new Curso();
+            curso2.setTitulo("curso js");
+            curso2.setDescricao("descrição curso js");
+            curso2.setCargaHoraria(4);
+
+            Mentoria mentoria = new Mentoria();
+            mentoria.setTitulo("mentoria de java");
+            mentoria.setDescricao("descricao mentoria java");
+            mentoria.setData(LocalDate.now());
+
+//            System.out.println(curso1);
+//            System.out.println(curso2);
+//            System.out.println(mentoria);
+
+            Bootcamp bootcamp = new Bootcamp();
+            bootcamp.setNome("Bootcamp Java Developer");
+            bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+            bootcamp.getConteudos().add(curso1);
+            bootcamp.getConteudos().add(curso2);
+            bootcamp.getConteudos().add(mentoria);
+
+            Dev dev1 = new Dev();
+            dev1.setNome("Flávio Brito");
+            dev1.inscreverBootcamp(bootcamp);
+            System.out.println("Matrícula efetuada com sucesso!");
+            System.out.println("Conteúdos inscritos Flávio Brito: " + dev1.getConteudosInscritos());
+            System.out.println("_");
+            dev1.progredir();
+            dev1.progredir();
+            System.out.println("Conteúdos inscritos Flávio Brito: " + dev1.getConteudosInscritos());
+            System.out.println("Conteúdos concluídos Flávio Brito: " + dev1.getConteudosConcluidos());
+            System.out.println("XP: " + dev1.calcularTotalXp());
+
+            System.out.println("_______________________________________________");
+
+            Dev dev2 = new Dev();
+            dev2.setNome("João Vitor");
+            dev2.inscreverBootcamp(bootcamp);
+            System.out.println("Matrícula efetuada com sucesso!");
+            System.out.println("Conteúdos inscritos João Vitor: " + dev2.getConteudosInscritos());
+            System.out.println("_");
+            dev2.progredir();
+            dev2.progredir();
+            dev2.progredir();
+            System.out.println("Conteúdos inscritos João Vitor: " + dev2.getConteudosInscritos());
+            System.out.println("Conteúdos concluídos João Vitor: " + dev2.getConteudosConcluidos());
+            System.out.println("XP: " + dev2.calcularTotalXp());
+
+    }
     }
